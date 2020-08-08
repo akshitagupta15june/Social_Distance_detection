30baf5ee3702014fab113b192754fcf8a187f072
akshitagupta15june <akshitagupta15june@gmail.com>
2020-08-08 10:43:24 +0530

    person_detected

--- person_detection.py
+++ person_detection.py
@@ -10,7 +10,7 @@ CLASSES=["background", "aeroplane", "bicycle", "bird", "boat",
 "sofa", "train", "tvmonitor"]
 
 image=cv2.imread('people.jpg')
-image=imutils.resize(image,width=800)
+image=imutils.resize(image,width=600)
 (H,W)=image.shape[:2]
 blob=cv2.dnn.blobFromImage(image,0.007843,(H,W),127.5)
 detector.setInput(blob)
@@ -24,6 +24,8 @@ for i in np.arange(0,person_detections.shape[2]):
         person_box=person_detections[0,0,i,3:7]*np.array([W,H,W,H])
         (startX,startY,endX,endY)=person_box.astype("int")
         cv2.rectangle(image,(startX,startY),(endX,endY),(0,255,0),2)
+#cv2.imwrite("person detected",image)
 cv2.imshow("results",image)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
